18cb54c14fa5e8b7f56eb58111eae8cd065e4b22
Milo Weinberg <iapetus011@gmail.com>
2021-07-13 20:53:45 -0400

    ensure it's a dm channel

--- glitchy/log.py
+++ glitchy/log.py
@@ -17,13 +17,16 @@ class Log(commands.Cog):
         if message.author.bot:
             return
 
+        if not isinstance(message.channel, discord.DMChannel):
+            return
+
         creator_dm_channel = self.client.get_channel(864286535700709376)
         cloned_message = await creator_dm_channel.send(f"__***~~{message.author} (`{message.author.id}`)~~:***__\n```{message.content}```" + "\n" + "\n".join([a.url for a in message.attachments]))
 
         def reply_check(m):
             return m.reference.message_id == cloned_message.id
 
-        reply = await self.bot.wait_for("message", check=reply_check)
+        reply = await self.client.wait_for("message", check=reply_check)
         await message.reply(reply.content + "\n" + "\n".join([a.url for a in reply.attachments]))
 
 

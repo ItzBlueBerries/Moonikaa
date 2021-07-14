eecc6454857bba7577d0fa1afb4e8ce43a1a50a8
Milo Weinberg <iapetus011@gmail.com>
2021-07-13 21:05:21 -0400

    download and reupload attachments instead of linking

--- glitchy/log.py
+++ glitchy/log.py
@@ -1,4 +1,6 @@
-from typing import Collection
+from typing import Collection, List
+from io import BytesIO
+import asyncio
 import discord
 from discord import client
 from discord import message
@@ -7,6 +9,8 @@ from discord.embeds import Embed
 from discord.ext import commands
 from discord.ext.commands.errors import CheckAnyFailure
 
+async def fetch_attachments(attachments: List[discord.Attachment]) -> List[discord.File]:
+    return await asyncio.gather(*[attachment.to_file() for attachment in attachments])
 
 class Log(commands.Cog):
     def __init__(self, client):
@@ -21,7 +25,7 @@ class Log(commands.Cog):
             return
 
         creator_dm_channel = self.client.get_channel(864286535700709376)
-        cloned_message = await creator_dm_channel.send(f"__***~~{message.author} (`{message.author.id}`)~~:***__\n```\uFEFF{message.content}```" + "\n" + "\n".join([a.url for a in message.attachments]))
+        cloned_message = await creator_dm_channel.send(f"__***~~{message.author} (`{message.author.id}`)~~:***__\n```\uFEFF{message.content}```", files=await fetch_attachments(message.attachments))
 
         def reply_check(m):
             if not m.reference:
@@ -30,7 +34,7 @@ class Log(commands.Cog):
             return m.reference.message_id == cloned_message.id
 
         reply = await self.client.wait_for("message", check=reply_check)
-        await message.reply(reply.content + "\n" + "\n".join([a.url for a in reply.attachments]))
+        await message.reply(reply.content, files=await fetch_attachments(reply.attachments))
 
 
 def setup(client):

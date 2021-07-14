288d23dcb50a4fa18e8cc387df9f74a599bbf8f6
Milo Weinberg <iapetus011@gmail.com>
2021-07-13 20:32:02 -0400

    format with black

--- glitch.py
+++ glitch.py
@@ -11,28 +11,32 @@ GlitchyPrefix = os.getenv("GLITCHY_PREFIX")
 
 client = commands.Bot(command_prefix=GlitchyPrefix + " ")
 
-for filename in os.listdir('./glitchy'):
-    if filename.endswith('.py'):
-        client.load_extension(f'glitchy.{filename[:-3]}')
+for filename in os.listdir("./glitchy"):
+    if filename.endswith(".py"):
+        client.load_extension(f"glitchy.{filename[:-3]}")
+
 
 @client.command()
 @commands.is_owner()
 async def load(ctx, extension):
-    client.load_extension(f'glitchy.{extension}')
-    await ctx.send(f'{extension} has been glitchified. :)')
+    client.load_extension(f"glitchy.{extension}")
+    await ctx.send(f"{extension} has been glitchified. :)")
+
 
 @client.command()
 @commands.is_owner()
 async def unload(ctx, extension):
-    client.unload_extension(f'glitchy.{extension}')
-    await ctx.send(f'{extension} has been unglitchified. :)')
+    client.unload_extension(f"glitchy.{extension}")
+    await ctx.send(f"{extension} has been unglitchified. :)")
+
 
 @client.command()
 @commands.is_owner()
 async def reload(ctx, extension):
-    client.unload_extension(f'glitchy.{extension}')
-    client.load_extension(f'glitchy.{extension}')
-    await ctx.send(f'{extension} has been reglitchified. :)')
+    client.unload_extension(f"glitchy.{extension}")
+    client.load_extension(f"glitchy.{extension}")
+    await ctx.send(f"{extension} has been reglitchified. :)")
+
 
 ## Testing Events
 
--- glitchy/log.py
+++ glitchy/log.py
@@ -7,11 +7,10 @@ from discord.embeds import Embed
 from discord.ext import commands
 from discord.ext.commands.errors import CheckAnyFailure
 
-class Log(commands.Cog):
 
+class Log(commands.Cog):
     def __init__(self, client):
         self.client = client
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -26,15 +25,15 @@ class Log(commands.Cog):
         if str(message.channel.type) == "private":
             if message.attachments != empty:
                 filee = message.attachments[-1]
-                await message.author.send('You\'re DM was successfully sent to the creators, thank you! :)')
-                await creator_dm.send(f'__***~~ {message.author} ({message.author.id}) ~~ Sent:***__')
+                await message.author.send("You're DM was successfully sent to the creators, thank you! :)")
+                await creator_dm.send(f"__***~~ {message.author} ({message.author.id}) ~~ Sent:***__")
 
                 for file in filee:
-                    await creator_dm.send(f'{filee.url}')
+                    await creator_dm.send(f"{filee.url}")
             # creator_dm = discord.utils.get(self.client.get_all_channels(), guild__name='Fruitsy Bot Testing Server', name='dms-for-the-creator')
             else:
-                await message.author.send('You\'re DM was successfully sent to the creators, thank you! :)')
-                await creator_dm.send(f'__***~~ {message.author} ({message.author.id}) ~~ Sent:***__\n```{message.content}```')
+                await message.author.send("You're DM was successfully sent to the creators, thank you! :)")
+                await creator_dm.send(f"__***~~ {message.author} ({message.author.id}) ~~ Sent:***__\n```{message.content}```")
         # elif str(message.channel) == channel and message.content.startsWith("<"):
         #     member_obj = message.mentions[0]
 
@@ -43,6 +42,7 @@ class Log(commands.Cog):
         #     dm_message = string[index:]
 
         #     await member_obj.send(f'__***~~ {message.author} ({message.author.id}) ~~ Sent Back:***__\n```{dm_message}```')
+
 
 def setup(client):
     client.add_cog(Log(client))
